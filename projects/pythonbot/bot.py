4f84379391cf4262074e298d5c0fe93df94443fc
Igor <igoryarm2@yandex.ru>
2019-05-24 17:21:59 +0300

    Бот для телеги первый в жизни

---
+++ projects/pythonbot/bot.py
@@ -0,0 +1,14 @@
+from telegram.ext import Updater, CommandHandler
+PROXY = {'proxy_url': 'socks5://t3.learn.python.ru:1080',
+    'urllib3_proxy_kwargs': {'username': 'learn', 'password': 'python'}}
+def greet_user(bot, update):
+    text = 'Вызван /start'
+    print(text)
+    update.message.reply_text(text)
+def main():
+    mybot = Updater("768453628:AAHeNvlEQqNG6i2c8VgVvPmjhDRO32O7O5o", request_kwargs=PROXY)
+    dp = mybot.dispatcher
+    dp.add_handler(CommandHandler("start", greet_user))
+    mybot.start_polling()
+    mybot.idle()
+main()
